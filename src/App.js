3d9d3e327dc99538a79f8178808a383b50a5959c
Ananya-dozakh-survivor <itsaananyamannaa@gmail.com>
2025-06-22 11:48:20 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 // import logo from './logo.svg';
 import Alert from './components/Alert';
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
 
 // import React from "react";
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
 
-// } from "react-router-dom";
+} from "react-router-dom";
 
 
 function App() {
@@ -35,34 +35,34 @@ function App() {
       setMode ( 'dark')
       document.body.style.backgroundColor='#042743';
       showAlert("Dark mode has been enabled", "success")
-      document.title = 'TextUtils - Dark Mode';
+      // document.title = 'TextUtils - Dark Mode';
     }
     else{
       setMode ('light')
       document.body.style.backgroundColor='white';
       showAlert("Light mode has been enabled", "success")
-      document.title = 'TextUtils - Light Mode';
+      // document.title = 'TextUtils - Light Mode';
     }
   };
   return (
   <>
-{/* <Navbar title="TextUtils" aboutText= "About TextUtils"/> */}
-{/* <Navbar/> */}
-{/* <Router> */}
+{/* <Navbar title="TextUtils" aboutText= "About TextUtils"/>
+<Navbar/> */}
+<Router>
 <Navbar title="TextUtils" mode={mode}  toggleMode={toggleMode} />
 <Alert alert={alert}/>
 <div className="container my-3">
-   {/* <Routes> */}
+   <Routes>
           {/* <Route exact path="/about"> */}
-               {/* <About /> */}
-          {/* <Route path="/about" element={<About />} /> */}
+               {/* <About  mode={mode} /> */}
+          <Route path="/about" element={<About  mode={mode}/>} />
           {/* <Route exact path="/"> */}
-          <TextForm showAlert={showAlert} heading = "Enter the text to analyse below" mode={mode} />   
-          {/* <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyse below" mode={mode} />} /> */}
-   {/* </Routes> */}
+          {/* <TextForm showAlert={showAlert} heading = "Enter the text to analyse below" mode={mode} />    */}
+          <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyse below" mode={mode} />} />
+   </Routes>
    
 </div>
-{/* </Router> */}
+</Router>
 
      </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-// import {a} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 
 export default function Navbar(props) {
   return (
       <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">{props.title}</a>
+   <Link className="navbar-brand" to="/">{props.title}</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-a active" aria-current="page" href="#">Home</a>
+        <Link className="nav-link active" aria-current="page" to="/">Home</Link>
         </li>
-        {/* <li className="nav-item">
-          <a className="nav-a" href="/about">About</a>
-        </li> */}
+        <li className="nav-item">
+         <Link className="nav-link" to="/about">About</Link>
+        </li>
         {/* <li className="nav-item dropdown">
-          <a className="nav-a dropdown-toggle" to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+         link className="nav-a dropdown-toggle" to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
           <ul className="dropdown-menu">
@@ -31,7 +31,7 @@ export default function Navbar(props) {
           </ul>
         </li>
         <li className="nav-item">
-          <a className="nav-a disabled" aria-disabled="true">Disabled</a>
+         link className="nav-a disabled" aria-disabled="true">Disabled</a>
         </li> */}
       </ul>
       {/* <form className="d-flex" role="search">
